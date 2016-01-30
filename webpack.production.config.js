662dec48707f302b3d3aad64e744981e71cd8e3b
Allen Eubank <adeubank@gmail.com>
2016-01-30 13:22:15 -0800

    add cache busting production configurations

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,6 +2,9 @@ var webpack = require('webpack');
 var config = require('./webpack.config.js');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+// update the output files to use hashes for cache busting
+config.output.filename = '[name]-[hash].js';
+
 // modify the styles loader to include the extract text plugin so that our
 // style sheets are in a separate file and not apart of the bundle
 var styleLoader = config.module.loaders.find((ldr) => ldr['id'] === 'styles');
@@ -11,7 +14,7 @@ styleLoader.loader = ExtractTextPlugin.extract('style', 'css?modules!postcss');
 config.plugins.push(
   new webpack.optimize.OccurenceOrderPlugin(),
   new webpack.optimize.UglifyJsPlugin(),
-  new ExtractTextPlugin('style.css')
+  new ExtractTextPlugin('[name]-[hash].css')
 );
 
 module.exports = config;
